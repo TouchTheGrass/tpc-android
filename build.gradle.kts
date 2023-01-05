50237b48f76a20dd3c34e0592b8436f6252d815f
Reversean <odaving111@gmail.com>
2023-01-05 15:23:00 +0300

    Basic android app

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+    }
+}
+
+//plugins {
+//    id("com.android.library") version "7.3.0"
+//    id("com.android.application") version "7.3.0"
+//    id("org.jetbrains.kotlin.android") version "1.7.0"
+//}
